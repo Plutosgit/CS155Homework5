2a5cfef05d15c47f6dc57ae7339f79667bf49752
Prith Balaji <prithvibalaji16@gmail.com>
2021-09-27 19:08:25 -0700

    Add project files.

---
+++ CS155Homework5Part1/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CS155Homework5Part1
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int BMI_GOOD_RANGE_LOW  = 15;
+        const int BMI_GOOD_RANGE_HIGH = 26;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Title = "BMI Calculator";
+
+            txtWeight.Focus();
+
+        }
+
+
+        private void txtHeight_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtHeight.SelectAll();
+        }
+
+        private void txtWeight_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtWeight.SelectAll();
+        }
+
+        private void cmdCalcBMI_OnClick(object sender, RoutedEventArgs e)
+        {
+            double dWeight, dHeight, dBMI;
+            bool bResult;
+
+            bResult = Double.TryParse(txtWeight.Text, out dWeight);
+            if (!bResult)
+            {
+                MessageBox.Show("Please enter a valid Weight!");
+                return;
+            }
+
+            bResult = Double.TryParse(txtHeight.Text, out dHeight);
+            if (!bResult)
+            {
+                MessageBox.Show("Please enter a valid Height!");
+                return;
+            }
+
+            dBMI = dWeight * 720.0 / (dHeight * dHeight);
+            String sdBMI = String.Format("{0:F1}", dBMI);
+
+            if (dBMI >= BMI_GOOD_RANGE_LOW && dBMI <= BMI_GOOD_RANGE_HIGH)
+            {
+                lblBMIOutput.Content = "Your BMI : " + sdBMI + " is excellent!";
+                lblBMIOutput.Background = Brushes.Green;
+            }
+            else if(dBMI < BMI_GOOD_RANGE_LOW)
+            {
+                lblBMIOutput.Content = "Your BMI : " + sdBMI + " is LOW!";
+                lblBMIOutput.Background = Brushes.Blue;
+
+            }
+            else 
+            {
+                lblBMIOutput.Content = "Your BMI : " + sdBMI + " is HIGH!";
+                lblBMIOutput.Background = Brushes.Yellow;
+
+                                                                                                                    }
+        }
+
+        private void txtWeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (lblBMIOutput != null)
+            {
+                lblBMIOutput.Content = "";
+                lblBMIOutput.Background = Brushes.White; // canvasMain.Background;
+            }
+
+        }
+
+        private void txtHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (lblBMIOutput != null)
+            { 
+                lblBMIOutput.Content = "";
+                lblBMIOutput.Background =  Brushes.White; // canvasMain.Background;
+            }
+        }
+    }
+}
---
+++ CS155Homework5Part2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CS155Homework5Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Homework 5 Part 2 Coin Toss\n");
+
+            int numTosses = 0, 
+                numHeads = 0, 
+                numTails = 0,
+                nthToss = 0;
+
+            char userkey;
+
+            Console.WriteLine("-------------INPUT--------------");
+
+            while (numTosses < 8)
+            {
+                nthToss = numTosses + 1;
+                Console.Write("\nToss # " + nthToss.ToString() + " - Enter head or tail: ");
+                userkey = Console.ReadKey().KeyChar;
+                if (userkey.ToString().ToLower() == "h")
+                {
+                    numHeads++;
+                    numTosses++;
+                }
+                if (userkey.ToString().ToLower() == "t")
+                {   numTails++;
+                    numTosses++;
+                }
+
+            }
+
+            double dpctHeads, dpctTails;
+            dpctHeads = ((double)numHeads / (double)numTosses)*100.0;
+            dpctTails = ((double)numTails / (double) numTosses)*100.0;
+
+            String spctHeads = String.Format("{0:F1}", dpctHeads);
+            String spctTails = String.Format("{0:F1}", dpctTails);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("-------------OUTPUT-------------\n");
+
+            Console.WriteLine("Total number of tosses : " + numTosses.ToString());
+            Console.WriteLine("Total number of heads  : " + numHeads.ToString());
+            Console.WriteLine("Total number of tails  : " + numTails.ToString());
+            Console.WriteLine("% Heads : " + spctHeads);
+            Console.WriteLine("% Tails : " + spctTails);
+
+        }
+    }
+}
